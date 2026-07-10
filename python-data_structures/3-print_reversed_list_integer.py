3f65abb97624d322196b267b48c4430f8ba669d8
Isimbi-Nina <h.isimbi@alustudent.com>
2026-07-10 10:47:21 -0700

    Correct error

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -4,6 +4,9 @@
 
 def print_reversed_list_integer(my_list=[]):
     """Print all integers of a list in reverse order, one per line."""
+    if my_list is None:
+        return
+
     i = len(my_list) - 1
     while i >= 0:
         print("{:d}".format(my_list[i]))
